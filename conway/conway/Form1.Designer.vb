7efa6bb13a838bd92ca94dbeaa9b4f90a42a6a6d
isaacthoman <isaacthoman@outlook.com>
2021-01-07 16:45:19 -0500

    Finished save system and added presets

--- conway/conway/Form1.Designer.vb
+++ conway/conway/Form1.Designer.vb
@@ -23,10 +23,10 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btnIterate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
         Me.iterationLabel = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
@@ -34,7 +34,7 @@ Partial Class Form1
         Me.RadioButton20 = New System.Windows.Forms.RadioButton()
         Me.RadioButton10 = New System.Windows.Forms.RadioButton()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.RadioButton5 = New System.Windows.Forms.RadioButton()
         Me.RadioButton4 = New System.Windows.Forms.RadioButton()
@@ -44,52 +44,61 @@ Partial Class Form1
         Me.CheckBox3 = New System.Windows.Forms.CheckBox()
         Me.CheckBox2 = New System.Windows.Forms.CheckBox()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.Button6 = New System.Windows.Forms.Button()
+        Me.btnRefresh = New System.Windows.Forms.Button()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button8 = New System.Windows.Forms.Button()
+        Me.btnSave_debug = New System.Windows.Forms.Button()
+        Me.saveLoadTextBox = New System.Windows.Forms.TextBox()
+        Me.btnLoad_debug = New System.Windows.Forms.Button()
         Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton8 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnIterate
         '
-        Me.Button1.Location = New System.Drawing.Point(25, 44)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Iterate"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnIterate.Location = New System.Drawing.Point(25, 44)
+        Me.btnIterate.Name = "btnIterate"
+        Me.btnIterate.Size = New System.Drawing.Size(75, 23)
+        Me.btnIterate.TabIndex = 0
+        Me.btnIterate.Text = "Iterate"
+        Me.btnIterate.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnClear
         '
-        Me.Button2.Location = New System.Drawing.Point(25, 73)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Clear"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnClear.Location = New System.Drawing.Point(25, 73)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnStop
         '
-        Me.Button3.Location = New System.Drawing.Point(123, 73)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Stop"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnStop.Location = New System.Drawing.Point(106, 73)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 2
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btnStart
         '
-        Me.Button4.Location = New System.Drawing.Point(123, 44)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Start"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnStart.Location = New System.Drawing.Point(106, 44)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 3
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
         '
         'iterationLabel
         '
@@ -110,7 +119,7 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.RadioButton20)
         Me.GroupBox1.Controls.Add(Me.RadioButton10)
         Me.GroupBox1.Controls.Add(Me.RadioButton1)
-        Me.GroupBox1.Location = New System.Drawing.Point(25, 172)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 181)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(173, 123)
         Me.GroupBox1.TabIndex = 5
@@ -162,14 +171,14 @@ Partial Class Form1
         Me.RadioButton1.Text = "1 frame/sec"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
-        'Button5
+        'btnExit
         '
-        Me.Button5.Location = New System.Drawing.Point(25, 102)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
-        Me.Button5.TabIndex = 6
-        Me.Button5.Text = "Exit"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btnExit.Location = New System.Drawing.Point(25, 102)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'GroupBox2
         '
@@ -177,7 +186,7 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.RadioButton4)
         Me.GroupBox2.Controls.Add(Me.RadioButton3)
         Me.GroupBox2.Controls.Add(Me.RadioButton2)
-        Me.GroupBox2.Location = New System.Drawing.Point(25, 301)
+        Me.GroupBox2.Location = New System.Drawing.Point(25, 310)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(173, 119)
         Me.GroupBox2.TabIndex = 8
@@ -219,7 +228,7 @@ Partial Class Form1
         '
         Me.RadioButton2.AutoSize = True
         Me.RadioButton2.Checked = True
-        Me.RadioButton2.Location = New System.Drawing.Point(6, 20)
+        Me.RadioButton2.Location = New System.Drawing.Point(6, 19)
         Me.RadioButton2.Name = "RadioButton2"
         Me.RadioButton2.Size = New System.Drawing.Size(70, 17)
         Me.RadioButton2.TabIndex = 0
@@ -232,7 +241,7 @@ Partial Class Form1
         Me.GroupBox4.Controls.Add(Me.CheckBox3)
         Me.GroupBox4.Controls.Add(Me.CheckBox2)
         Me.GroupBox4.Controls.Add(Me.CheckBox1)
-        Me.GroupBox4.Location = New System.Drawing.Point(25, 426)
+        Me.GroupBox4.Location = New System.Drawing.Point(25, 435)
         Me.GroupBox4.Name = "GroupBox4"
         Me.GroupBox4.Size = New System.Drawing.Size(173, 100)
         Me.GroupBox4.TabIndex = 10
@@ -268,87 +277,180 @@ Partial Class Form1
         Me.CheckBox1.AutoSize = True
         Me.CheckBox1.Checked = True
         Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.CheckBox1.Location = New System.Drawing.Point(7, 20)
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 20)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(157, 17)
         Me.CheckBox1.TabIndex = 0
         Me.CheckBox1.Text = "Death from underpopulation"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
-        'Button6
+        'btnRefresh
         '
-        Me.Button6.Location = New System.Drawing.Point(123, 102)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(75, 23)
-        Me.Button6.TabIndex = 11
-        Me.Button6.Text = "Refresh"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.btnRefresh.Location = New System.Drawing.Point(106, 102)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.btnRefresh.TabIndex = 11
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
         '
         'GroupBox3
         '
-        Me.GroupBox3.Location = New System.Drawing.Point(25, 542)
+        Me.GroupBox3.Controls.Add(Me.RadioButton8)
+        Me.GroupBox3.Controls.Add(Me.RadioButton6)
+        Me.GroupBox3.Location = New System.Drawing.Point(25, 544)
         Me.GroupBox3.Name = "GroupBox3"
         Me.GroupBox3.Size = New System.Drawing.Size(173, 105)
         Me.GroupBox3.TabIndex = 12
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Presets"
         '
-        'Button7
+        'btnSave_debug
         '
-        Me.Button7.Location = New System.Drawing.Point(564, 542)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(75, 23)
-        Me.Button7.TabIndex = 13
-        Me.Button7.Text = "Save"
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.btnSave_debug.Location = New System.Drawing.Point(592, 569)
+        Me.btnSave_debug.Name = "btnSave_debug"
+        Me.btnSave_debug.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave_debug.TabIndex = 13
+        Me.btnSave_debug.Text = "Save data"
+        Me.btnSave_debug.UseVisualStyleBackColor = True
+        Me.btnSave_debug.Visible = False
         '
-        'TextBox1
+        'saveLoadTextBox
         '
-        Me.TextBox1.Location = New System.Drawing.Point(645, 544)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(227, 125)
-        Me.TextBox1.TabIndex = 14
+        Me.saveLoadTextBox.Location = New System.Drawing.Point(673, 570)
+        Me.saveLoadTextBox.Multiline = True
+        Me.saveLoadTextBox.Name = "saveLoadTextBox"
+        Me.saveLoadTextBox.Size = New System.Drawing.Size(199, 79)
+        Me.saveLoadTextBox.TabIndex = 14
+        Me.saveLoadTextBox.Visible = False
         '
-        'Button8
+        'btnLoad_debug
         '
-        Me.Button8.Location = New System.Drawing.Point(564, 571)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(75, 23)
-        Me.Button8.TabIndex = 15
-        Me.Button8.Text = "Load"
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.btnLoad_debug.Location = New System.Drawing.Point(592, 598)
+        Me.btnLoad_debug.Name = "btnLoad_debug"
+        Me.btnLoad_debug.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoad_debug.TabIndex = 15
+        Me.btnLoad_debug.Text = "Load data"
+        Me.btnLoad_debug.UseVisualStyleBackColor = True
+        Me.btnLoad_debug.Visible = False
         '
         'CheckBox4
         '
         Me.CheckBox4.AutoSize = True
-        Me.CheckBox4.Location = New System.Drawing.Point(204, 44)
+        Me.CheckBox4.Location = New System.Drawing.Point(25, 158)
         Me.CheckBox4.Name = "CheckBox4"
-        Me.CheckBox4.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox4.Size = New System.Drawing.Size(103, 17)
         Me.CheckBox4.TabIndex = 16
-        Me.CheckBox4.Text = "CheckBox4"
+        Me.CheckBox4.Text = "Show save data"
         Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(6, 25)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(73, 17)
+        Me.RadioButton6.TabIndex = 0
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "Oscillators"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton8
+        '
+        Me.RadioButton8.AutoSize = True
+        Me.RadioButton8.Location = New System.Drawing.Point(6, 46)
+        Me.RadioButton8.Name = "RadioButton8"
+        Me.RadioButton8.Size = New System.Drawing.Size(80, 17)
+        Me.RadioButton8.TabIndex = 2
+        Me.RadioButton8.TabStop = True
+        Me.RadioButton8.Text = "Spaceships"
+        Me.RadioButton8.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(204, 558)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(204, 587)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(204, 616)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 19
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(285, 558)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(285, 587)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 21
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.Visible = False
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(285, 616)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 22
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        Me.Button6.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(884, 679)
-        Me.Controls.Add(Me.CheckBox4)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.Button6)
-        Me.Controls.Add(Me.GroupBox4)
-        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.iterationLabel)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.btnLoad_debug)
+        Me.Controls.Add(Me.saveLoadTextBox)
+        Me.Controls.Add(Me.btnSave_debug)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.iterationLabel)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnIterate)
         Me.Name = "Form1"
         Me.Text = "Game of Life"
         Me.GroupBox1.ResumeLayout(False)
@@ -357,15 +459,17 @@ Partial Class Form1
         Me.GroupBox2.PerformLayout()
         Me.GroupBox4.ResumeLayout(False)
         Me.GroupBox4.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
+    Friend WithEvents btnIterate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnStart As Button
     Friend WithEvents iterationLabel As Label
     Friend WithEvents Timer1 As Timer
     Friend WithEvents GroupBox1 As GroupBox
@@ -373,7 +477,7 @@ Partial Class Form1
     Friend WithEvents RadioButton20 As RadioButton
     Friend WithEvents RadioButton10 As RadioButton
     Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents Button5 As Button
+    Friend WithEvents btnExit As Button
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents RadioButton5 As RadioButton
     Friend WithEvents RadioButton4 As RadioButton
@@ -383,10 +487,18 @@ Partial Class Form1
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents CheckBox3 As CheckBox
     Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents Button6 As Button
+    Friend WithEvents btnRefresh As Button
     Friend WithEvents GroupBox3 As GroupBox
-    Friend WithEvents Button7 As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button8 As Button
+    Friend WithEvents btnSave_debug As Button
+    Friend WithEvents saveLoadTextBox As TextBox
+    Friend WithEvents btnLoad_debug As Button
     Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
